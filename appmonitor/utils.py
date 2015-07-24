08b21632d7600805b6c37e05b279e7f75a59573e
Jørgen Ulrik B. Krag <jubk@magenta.dk>
2015-07-24 08:37:01 +0200

    - Implement encryption utility methods

--- appmonitor/utils.py
+++ appmonitor/utils.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from email.header import Header
 from email.utils import parseaddr
-import smtplib
+import smtplib, md5, pyDes
 
 def get_smtp_server():
     if not settings.MAIL_FROM_EMAIL:
@@ -37,3 +37,34 @@ def make_email_header(email, name=None):
 
 def mimeencode_header(header_string):
     return Header(header_string, "utf-8").encode()
+
+# Implementation of windows CryptoAPI DeriveKey using MD5 algoritm.
+def derive_md5_key(key):
+    hash_val = bytearray(md5.new(key).digest())
+    buf1 = bytearray(b'\x36' * 64)
+    buf2 = bytearray(b'\x5c' * 64)
+    for x in range(len(hash_val)):
+        buf1[x] ^= hash_val[x]
+        buf2[x] ^= hash_val[x]
+    hash1 = md5.new(bytes(buf1)).digest()
+    hash2 = md5.new(bytes(buf2)).digest()
+    derived_key = hash1 + hash2
+
+    return derived_key[:24]
+
+# Provide a TripleDES object that uses the projects SECRET_KEY as encryption
+# key and encrypts/decrypts in the same ways as the windows CryptAPI used
+# by AutoIT.
+def make_tripleDES():
+    return pyDes.triple_des(
+        derive_md5_key(settings.SECRET_KEY),
+        pyDes.CBC,
+        IV="\0\0\0\0\0\0\0\0",
+        padmode=pyDes.PAD_PKCS5
+    )
+
+def encrypt_and_hex(data):
+    return make_tripleDES().encrypt(data).encode("hex")
+
+def decrypt_from_hex(data):
+    return make_tripleDES().decrypt(data.decode("hex"))
