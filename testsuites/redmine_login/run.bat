5573944c374516d4f78181c1de155d52cccb8497
Jørgen Ulrik B. Krag <jubk@magenta.dk>
2015-11-10 18:32:40 +0100

    - Remove redmine_login example test-suite

--- testsuites/redmine_login/run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-call %~dp0%..\..\configure_environment.bat
-python %~dp0%run.py
-pause
--- testsuites/redmine_login/run_autoit.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-call %~dp0%..\..\configure_environment.bat
-
-python -m appmonitor.autoit %~dp0%run.au3
-
-pause
