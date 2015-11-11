b45af943e571852d03205b005275af93dc0831f3
Jørgen Ulrik B. Krag <jubk@magenta-aps.dk>
2015-11-11 12:54:27 +0100

    - License and license boilerplate added - Fixed Excel download URLs so they do not collide with measure URLs

--- appmonitor/urls.py
+++ appmonitor/urls.py
@@ -1,3 +1,15 @@
+# The contents of this file are subject to the Mozilla Public License
+# Version 2.0 (the "License"); you may not use this file except in
+# compliance with the License. You may obtain a copy of the License at
+#    http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# Copyright 2015 Magenta Aps
+#
 from django.conf.urls import url
 from appmonitor.views import TestSuiteList, TestSuiteDetailView
 from appmonitor.views import TestSuiteDownloadView, MeasureView, MeasurePNGView
@@ -13,7 +25,7 @@ urlpatterns = [
     url(r'^testsuite/(?P<pk>[0-9]+)/(?P<mname>[^/]+)/?',
         MeasureView.as_view(),
         name="measure"),
-    url(r'^testsuite/(?P<pk>[0-9]+)/download/?',
+    url(r'^testsuite-download/(?P<pk>[0-9]+)/?',
         TestSuiteDownloadView.as_view(),
         name="testsuite_download"),
     url(r'^testsuite/(?P<pk>[0-9]+)/?',
